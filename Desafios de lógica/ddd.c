c906446ff020a3a40d9966d4c03e4c62788193a8
Enzo <enzogabriel8515@gmail.com>
2025-12-10 09:42:10 -0300

    Corrigindo pequeno detalhe

--- Desafios de lógica/ddd.c
+++ Desafios de lógica/ddd.c
@@ -2,29 +2,31 @@
 #include <stdbool.h>
 
 int DDD (int n){
-    char cidades[8][31] = {
-     "Brasilia","Salvador","Sao Paulo",
-     "Rio de Janeiro","Juiz de Fora","Campinas",
-     "Vitoria","Belo Horizonte"
-    };
+  char cidades[8][31] = {
+    "Brasilia","Salvador","Sao Paulo",
+    "Rio de Janeiro","Juiz de Fora","Campinas",
+    "Vitoria","Belo Horizonte"
+  };
     
-    int ddd [8] = {61,71,11,21,32,19,27,31};
+  int ddd [8] = {61,71,11,21,32,19,27,31};
     
-    bool achou = false;
-    int p=-1;
+  bool achou = false;
+  int p=-1;
     
-    for (int i = 0; i<8 && !achou ;++i){
-        if (n==ddd[i]){
-         achou = true;
-         p = i;
-        }
+  for (int i = 0; i<8 && !achou ;++i){
+    if (n==ddd[i]){
+      achou = true;
+      p = i;
     }
+  }
     
-    if (!achou){
-      return printf("DDD nao cadastrado\n");
-    }else{
-     return printf("%s\n", cidades[p]);  
-    }
+  if (!achou){
+   printf("DDD nao cadastrado\n");
+  }else{
+   printf("%s\n", cidades[p]);  
+  }
+  
+  return 0;
 }
  
 int main()
