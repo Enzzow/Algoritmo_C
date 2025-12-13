04b52c3588d5d35219f0826d7e2f510e3789fae2
Enzo <enzogabriel8515@gmail.com>
2025-12-13 11:06:05 -0300

    Adicionando pasta de Alocações dinâmicas

---
+++ Estrutura de dados e algoritmos/Alocações dinâmicas/Malloc/Malloc01.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main (){
+   
+    int n;
+    bool permitido = false;
+
+    while(!permitido){
+        printf(" Informe quantos números deseja armazenar: ");
+        scanf("%d",&n);
+
+        if (n<=0)
+          printf(" \n Inválido! \n");
+        else
+          printf(" \n Tamanho válido! \n");
+          permitido = true;
+    }
+
+    int *vetor = (int*) malloc(n* sizeof(int)); //Reservando memória
+
+    for (int i=0; i<n ;++i){
+        *(vetor+i) = i*3;
+    }
+
+    printf(" \n Valores do vetor: ");
+    for (int i=0; i<n ;++i){
+        printf("%d ",*(vetor+i));
+    }
+    printf("\n");
+
+    //*(vetor+i) --> dinâmica de vetor
+
+    free(vetor);
+
+    return 0;
+}
